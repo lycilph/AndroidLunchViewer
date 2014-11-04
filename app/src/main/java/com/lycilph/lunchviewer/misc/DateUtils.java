1c78cea95f04882298409d03e9bc7f7136c74b8b
lycilph <lycilph@gmail.com>
2014-11-04 20:50:35 +0100

    Small update

--- app/src/main/java/com/lycilph/lunchviewer/misc/DateUtils.java
+++ app/src/main/java/com/lycilph/lunchviewer/misc/DateUtils.java
@@ -1,25 +1,25 @@
 package com.lycilph.lunchviewer.misc;
 
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeConstants;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
+
+import static org.joda.time.DateTimeConstants.MONDAY;
 
 public class DateUtils {
     public static LocalDate getNextValidDate() {
         LocalTime lunchEnd = DateTime.now().withTime(13, 15, 0, 0).toLocalTime();
         LocalTime now = DateTime.now().toLocalTime();
         int dayOfWeek = DateTime.now().toLocalDate().dayOfWeek().get();
+        boolean isAfterLunch = now.isAfter(lunchEnd);
 
-        if (dayOfWeek > 5) {
-            LocalDate mondayOfNextWeek = DateTime.now().plusWeeks(1).withDayOfWeek(DateTimeConstants.MONDAY).toLocalDate();
-            return mondayOfNextWeek;
-        }
+        if (dayOfWeek > 5 || (dayOfWeek == 5 && isAfterLunch))
+            return DateTime.now().plusWeeks(1).withDayOfWeek(MONDAY).toLocalDate();
 
-        if (now.isBefore(lunchEnd))
-            return DateTime.now().toLocalDate();
-        else
+        if (isAfterLunch)
             return DateTime.now().plusDays(1).toLocalDate();
+        else
+            return DateTime.now().toLocalDate();
     }
 
     public static int getYear() {
